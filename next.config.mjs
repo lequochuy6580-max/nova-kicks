b5021e144244d18457342920535f030485ccb5fb
lehuy <lequochuy6580@gmail.com>
2026-06-11 19:04:41 +0700

    fix: remove turbopack config

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,3 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactCompiler: true,
-
-  // Cho phép load ảnh từ các domain bên ngoài
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'myshoes.vn',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.unsplash.com',
-      },
-    ],
-  },
-};
-
+﻿/** @type {import("next").NextConfig} */
+const nextConfig = {};
 export default nextConfig;
